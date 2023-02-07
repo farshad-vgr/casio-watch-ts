97976e30b4e792d72489a1eb7d538dbb5e4fdb56
farshad-vgr <farshadvaghari@gmail.com>
2023-02-07 18:37:12 +0330

    Comments added

--- index.js
+++ index.js
@@ -5,8 +5,10 @@ var hourElement = document.querySelector(".hour");
 var dateElement = document.querySelector(".date");
 var dayElement = document.querySelector(".day");
 var displayElement = document.querySelector(".display");
+// This function updates time and date values
 function updateClock() {
     var currentTime = new Date();
+    // Extract time and date details
     var seconds = currentTime.getSeconds();
     var minutes = currentTime.getMinutes();
     var hours = currentTime.getHours();
@@ -14,17 +16,24 @@ function updateClock() {
     var day = currentTime.getDate();
     var month = currentTime.getMonth();
     var year = currentTime.getFullYear();
+    // Convert seconds value to a degree for the analog clock and set it as style for div element
     var secondsToDegree = (seconds / 60) * 360;
     secondElement.style.transform = "rotate(".concat(secondsToDegree, "deg)");
+    // Convert minutes value to a degree for the analog clock and set it as style for div element
     var minutesToDegree = (minutes / 60) * 360;
     minuteElement.style.transform = "rotate(".concat(minutesToDegree, "deg)");
+    // Convert hours value to a degree for the analog clock and set it as style for div element
     var hoursToDegree = (hours / 12) * 360 + minutes / 2;
     hourElement.style.transform = "rotate(".concat(hoursToDegree, "deg)");
+    // Convert the month value to the name of the month as a string
     var monthName = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Nov", "Dec"];
     dateElement.innerHTML = "".concat(year, " . ").concat(monthName[month], " . ").concat(day);
+    // Convert the day value to the name of the day as a string
     var DayName = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
     dayElement.innerHTML = "".concat(DayName[weekDay]);
+    // Set a string to know the time is in the morning or evening
     var AM_PM = "A M";
+    // This condition checks if time is one digit and then adds a 0 to the first and makes the time value from 0 to 12
     if (hours >= 22) {
         AM_PM = "P M";
         hours = hours - 12;
@@ -45,12 +54,16 @@ function updateClock() {
         AM_PM = "A M";
         hours = 12;
     }
+    // This condition checks if time is one digit and then adds a 0 to the first
     if (minutes < 10) {
         minutes = 0 + minutes;
     }
+    // This condition checks if time is one digit and then adds a 0 to the first
     if (seconds < 10) {
         seconds = 0 + seconds;
     }
+    // Set time values for the digital clock
     displayElement.innerHTML = "<span>".concat(hours, "</span> : <span>").concat(minutes, "</span> : <span>").concat(seconds, "</span>  <span><small><small>").concat(AM_PM, "</small></small></span>");
 }
+// Set an interval function to call the update function each second
 setInterval(updateClock, 1000);
--- index.ts
+++ index.ts
@@ -5,9 +5,11 @@ const dateElement = document.querySelector(".date") as HTMLDivElement;
 const dayElement = document.querySelector(".day") as HTMLDivElement;
 const displayElement = document.querySelector(".display") as HTMLDivElement;
 
+// This function updates time and date values
 function updateClock(): void {
   const currentTime: Date = new Date();
 
+  // Extract time and date details
   let seconds: number = currentTime.getSeconds();
   let minutes: number = currentTime.getMinutes();
   let hours: number = currentTime.getHours();
@@ -16,23 +18,30 @@ function updateClock(): void {
   let month: number = currentTime.getMonth();
   let year: number = currentTime.getFullYear();
 
+  // Convert seconds value to a degree for the analog clock and set it as style for div element
   const secondsToDegree: number = (seconds / 60) * 360;
   secondElement.style.transform = `rotate(${secondsToDegree}deg)`;
 
+  // Convert minutes value to a degree for the analog clock and set it as style for div element
   const minutesToDegree: number = (minutes / 60) * 360;
   minuteElement.style.transform = `rotate(${minutesToDegree}deg)`;
 
+  // Convert hours value to a degree for the analog clock and set it as style for div element
   const hoursToDegree: number = (hours / 12) * 360 + minutes / 2;
   hourElement.style.transform = `rotate(${hoursToDegree}deg)`;
 
+  // Convert the month value to the name of the month as a string
   const monthName: string[] = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Nov", "Dec"];
-  dateElement.innerHTML = `${year} . ${monthName[ month ]} . ${day}`;
+  dateElement.innerHTML = `${year} . ${monthName[month]} . ${day}`;
 
+  // Convert the day value to the name of the day as a string
   const DayName: string[] = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-  dayElement.innerHTML = `${DayName[ weekDay ]}`;
+  dayElement.innerHTML = `${DayName[weekDay]}`;
 
+  // Set a string to know the time is in the morning or evening
   let AM_PM: string = "A M";
 
+  // This condition checks if time is one digit and then adds a 0 to the first and makes the time value from 0 to 12
   if (hours >= 22) {
     AM_PM = "P M";
     hours = hours - 12;
@@ -50,15 +59,19 @@ function updateClock(): void {
     hours = 12;
   }
 
+  // This condition checks if time is one digit and then adds a 0 to the first
   if (minutes < 10) {
     minutes = 0 + minutes;
   }
 
+  // This condition checks if time is one digit and then adds a 0 to the first
   if (seconds < 10) {
     seconds = 0 + seconds;
   }
 
+  // Set time values for the digital clock
   displayElement.innerHTML = `<span>${hours}</span> : <span>${minutes}</span> : <span>${seconds}</span>  <span><small><small>${AM_PM}</small></small></span>`;
 }
 
+// Set an interval function to call the update function each second
 setInterval(updateClock, 1000);
